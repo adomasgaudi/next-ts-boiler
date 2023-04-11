a7fc7f4bfaf264d119bd312311caf6abde426d5a
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2023-04-11 14:53:53 +0300

    feat: copy legacy files

---
+++ legacy/index.tsx
@@ -0,0 +1,18 @@
+// import { OContainer, OButton } from "chunkyui";
+// import NextLink from "next/link";
+// import { concatStyleMulti } from "../../ossom/components/compBuilder";
+
+// export const Link = ({ children, href, cls }: any) => (
+//     <NextLink href={href}>
+//         <a className={cls}>{children}</a>
+//     </NextLink>
+// );
+
+// export const NavLink = concatStyleMulti(Link, {
+//     cls: "bg-red-400",
+// });
+
+// export const Button = OButton.wide;
+// export const Container = OContainer.tall;
+
+export {};
