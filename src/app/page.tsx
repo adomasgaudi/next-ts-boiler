851a6b4bba81b9f9d9cf9b5ef51b455e7a824f6f
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2023-05-01 14:14:22 +0300

    feat: add chroma-js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,30 @@
 'use client'
 /** @jsxImportSource @emotion/react */
 import { _ } from 'chainedcss'
+import chroma from 'chroma-js'
+import styled from 'styled-components'
 import { useCssOutline } from './utils/useTools'
+
+// import tw, { theme } from 'twin.macro'
+const startColor = chroma('#df8')
+const endColor = startColor.darken().hex()
+const DivGrad = styled.div`
+  background-image: linear-gradient(to right, ${endColor}, ${startColor});
+`
 
 function HomePage() {
   useCssOutline()
+
   return (
     <>
-      <div {..._.p2().wFull().hFull()} className="grad-4">
+      <DivGrad {..._.p2().wFull().hFull()} >
         <div tw="container m-auto py-10 border">
-          <div {..._.textCenter``.mt30``.bgBlue500``}>
+          <div {..._.textCenter``.mt30``.style(`background-image: linear-gradient(to right, ${startColor}, ${endColor})`)}>
             <h1 {..._.text(50).fontBold``}>The power of JS<br /> with the comfort of TailwindCSS</h1>
-
             <h3 {..._.textXl``.textWarmGray400``}>A new syntax for the Emotion css framework.</h3>
           </div>
         </div>
-      </div>
+      </DivGrad>
     </>
   )
 }
