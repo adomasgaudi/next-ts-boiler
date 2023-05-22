242ecca2931116c7dd8a642ace59b00efe9583e5
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2023-05-22 21:22:46 +0300

    feat: working gpt

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,103 +1,43 @@
 'use client'
-import { useRef, useState } from 'react'
-import useSWR from 'swr'
+import { useState } from 'react' // eslint-disable-next-lin
 
 const Form = () => {
-  const messageInput = useRef<HTMLTextAreaElement | null>(null)
   const [response, setResponse] = useState<string[]>([])
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [models, setModels] = useState<any>([])
-  const [currentModel, setCurrentModel] = useState<string>('gpt-3.5-turbo')
 
   const funcgo = async () => {
     const message = 'say hi in lithuanian'
-    if (message !== undefined) {
+
+    if (message) {
       setResponse(prev => [...prev, message])
-      messageInput.current!.value = ''
+
+      const response = await fetch('/api/response', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message }),
+      })
+
+      if (!response.ok)
+        throw new Error(response.statusText)
+
+      const data = await response.text()
+
+      if (data)
+        setResponse(prev => [...prev, data])
     }
-    if (!message)
-      return
-
-    const response = await fetch('/api/response', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        message: 'say hi in lithuanian',
-      }),
-    })
-    console.log('Edge function returned.')
-
-    console.log(response)
-
-    if (!response.ok)
-      throw new Error(response.statusText)
-
-    const data = response.body
-    if (!data)
-      return
-
-    const reader = data.getReader()
-    const decoder = new TextDecoder()
-    let done = false
-
-    setResponse(prev => [...prev, message])
-
-    let currentResponse: string[] = []
-    while (!done) {
-      const { value, done: doneReading } = await reader.read()
-      done = doneReading
-      const chunkValue = decoder.decode(value)
-      // currentResponse = [...currentResponse, message, chunkValue];
-      currentResponse = [...currentResponse, chunkValue]
-      setResponse(prev => [...prev.slice(0, -1), currentResponse.join('')])
-    }
-    // breaks text indent on refresh due to streaming
-    // localStorage.setItem('response', JSON.stringify(currentResponse));
-    setIsLoading(false)
   }
-
-  useSWR('fetchingResponse', async () => {
-    const storedResponse = localStorage.getItem('response')
-    if (storedResponse)
-      setResponse(JSON.parse(storedResponse))
-  })
-
-  const fetcher = async () => {
-    const models = await (await fetch('/api/models')).json()
-    setModels(models.data)
-    const modelIndex = models.data.findIndex(
-      (model: any) => model.id === 'gpt-3.5-turbo',
-    )
-    setCurrentModel(models.data[modelIndex].id)
-    return models
-  }
-
-  useSWR('fetchingModels', fetcher)
 
   console.log({ response })
 
   return (
     <div className='flex justify-center'>
-
-      <button onClick={funcgo}>submit now</button>
-
-      <form
-        onSubmit={() => {}}
-        className='fixed bottom-0 w-full md:max-w-3xl bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] mb-4'>
-        <textarea
-          name='Message'
-          placeholder='Type your query'
-          ref={messageInput}
-          className='w-full resize-none bg-transparent outline-none pt-4 pl-4 translate-y-1'/>
-        <button
-          disabled={isLoading}
-          type='submit'
-          className='border bg-white m-2'>
-          sumbit
-        </button>
-      </form>
+      <button className="border m-3 p-3 rounded-xl text-blue-800 bg-blue-300" onClick={funcgo}>submit now</button>
+      {response.length > 0 && (
+        <h3>
+          You asked: {response[0]} <br /> GPT Response: {response[1]}
+        </h3>
+      )}
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client'
-// eslint-disable-next-line
+
+import Link from 'next/link'// eslint-disable-next-line
 import tw from 'twin.macro'
 import Form from './components/Form'
 
-// const CardComp = ({ name, link, ...props }: any) => <Link href={`/${link}`}>
-//   <div tw="border p-10 inline m-10 rounded-xl" {...props}>{name}</div>
-// </Link>
+const CardComp = ({ name, link, ...props }: any) => <Link href={`/${link}`}>
+  <div tw="border p-10 inline m-10 rounded-xl" {...props}>{name}</div>
+</Link>
 
 function HomePage() {
   return (
@@ -16,9 +17,9 @@ function HomePage() {
           <h1>chat</h1>
         </section>
 
-        {/* <section>
+        <section>
           {[{ name: 'count of monte christo', link: 'count_of_monte_christo' }].map((_, idx) => <CardComp key={idx} name={_.name} link={_.link} />)}
-        </section> */}
+        </section>
         <section>
           <Form />
         </section>
